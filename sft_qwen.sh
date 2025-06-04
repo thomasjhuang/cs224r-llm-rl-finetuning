bd089d456ffe9bff609a96397b4302669c8c912f
yd <y@d.io>
2025-06-04 09:24:10 +0000

    wip

--- sft_qwen.sh
+++ sft_qwen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-python sft_qwen_torch.py   \
+python src/sft_qwen_torch.py   \
 --output_dir runs/qwen2-sft   \
---per_device_train_batch_size 4   \
---gradient_accumulation_steps 2   \
---epochs 3 \
+--per_device_train_batch_size 22   \
+--gradient_accumulation_steps 3   \
+--epochs 300 \
 --eval_every 5000 \
 --log_every 100 \
---save_every 5000
+--save_every 10000
